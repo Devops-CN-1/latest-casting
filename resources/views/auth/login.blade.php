e13080cff155c8716f1f81c1d8e8c3f3d408fc15
Ahmed-Developer <ahmed@creationnext.com>
2026-01-30 20:39:54 +0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
 
             <button type="submit"
                 class="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition">
-                Login
+                Login Test
             </button>
         </form>
     </div>
