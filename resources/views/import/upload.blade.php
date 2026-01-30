5a4a44f0e9dd56d51d12ae38fc3f89e0b098da47
Falaksher6510 <falak@creationnext.com>
2026-01-30 21:07:11 +0500

    Update upload.blade.php

--- resources/views/import/upload.blade.php
+++ resources/views/import/upload.blade.php
@@ -35,7 +35,7 @@
 
         <div class="flex gap-3 pt-2">
             <button type="submit" 
-                    class="px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition font-medium">
+                    class="px-6 py-2 bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-white rounded-lg hover:bg-gray-300 dark:hover:bg-gray-500 transition font-medium">
                 Upload & Import
             </button>
             <a href="{{ route('import.index') }}" 
