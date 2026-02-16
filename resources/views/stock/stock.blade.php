33b27563b2fa318ae11dbc2ea568b76d1e539e74
Falaksher6510 <falak@creationnext.com>
2026-02-16 23:17:45 +0500

    Update stock.blade.php

--- resources/views/stock/stock.blade.php
+++ resources/views/stock/stock.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto">
-        <div class="w-full p-2 bg-[#ece9d8] h-screen">
+        <div class="h-full w-full p-2 bg-[#ece9d8] h-screen">
             <!-- table container  -->
             <div
                 class="h-60 overflow-y-auto border-2 border-l-[#b1b0aa] border-t-[#c9c8c4] border-r-white border-b-white bg-[#808080]">
