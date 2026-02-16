c4569b9f16620384018fa669a72a335668a54c3e
Falaksher6510 <falak@creationnext.com>
2026-02-17 00:01:39 +0500

    Update create.blade.php

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -96,13 +96,13 @@
                     <!-- Labels Panel -->
                     <div class="w-1/2 flex flex-col items-center p-1 font-semibold bg-[#804000] text-[#f1da69]">
                         <span class="text-blue-100">Wapsi</span>
-                        <span class="text-blue-100 mt-2">Net Weight</span>
+                        <span class="text-blue-100 mt-6">Net Weight</span>
                         <span class="mt-6">ویسٹ</span>
                         <span class="mt-6">کُل وزن</span>
                         <span class="mt-6">میل نکالا</span>
-                        <span class="mt-6">خالص</span>
-                        <span class="mt-6">ایڈوانس</span>
-                        <span class="mt-6">کُل خالص</span>
+                        <span class="mt-5">خالص</span>
+                        <span class="mt-5">ایڈوانس</span>
+                        <span class="mt-5">کُل خالص</span>
                     </div>
                 </div>
                 <div class="w-[40%]">
