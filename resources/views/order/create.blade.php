28a963173dddfd928ee5cb253aa08f2b379cf3c2
Falaksher6510 <falak@creationnext.com>
2026-04-04 16:55:51 +0800

    Update create.blade.php

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -1365,11 +1365,7 @@ $(document).ready(function() {
 
 
 
-            let totalKhalis = khalis + advance;
-
-
-
-            $('#totalKhalis').val((Math.ceil(totalKhalis * 100) / 100).toFixed(3));
+            
             $('#netWeight').val(netWeight.toFixed(3));
             $('#wasteCasted').val(waste.toFixed(3));
             $('#totalWeight').val(totalWeight.toFixed(3));
@@ -1381,6 +1377,12 @@ $(document).ready(function() {
             var forKhalis = totalwaitforcountkhails - mailniklaforcountkhails;
 
             $('#khalis').val((Math.ceil(forKhalis * 100) / 100).toFixed(3));
+
+            let totalKhalis = khalis + advance;
+
+
+
+            $('#totalKhalis').val((Math.ceil(totalKhalis * 100) / 100).toFixed(3));
 
             // $('#khalis').val((totalwaitforcountkhails - mailniklaforcountkhails).toFixed(3));
 
